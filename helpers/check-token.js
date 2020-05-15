c9696ce0edd328d5079054ae29789567dfe02ece
Max Haponenko <gaponenko.mm@gmail.com>
2020-05-16 00:04:31 +0300

    Check toket server-side update

--- helpers/check-token.js
+++ helpers/check-token.js
@@ -4,43 +4,47 @@ const User = require('../models/User');
 
 // Middleware to varify users authentication access
 function checkToken(req, res, next) {
-    const authHeader = req.headers['authorization'];
-    if (typeof authHeader !== undefined) {
-        const bearer = authHeader.split(' ');
-        const accessToken = bearer[1];
-        
-        jwt.verify(accessToken, config.get('jwtSecret'), (err, decoded) => {
-            if (err) {
-                if (err.name === 'TokenExpiredError') {
-                    res.status(403).json({
-                        toastType: 4,
-                        toastMessage: 'Token expired Error'
-                    })
-                } else {
-                    res.status(403).json({
-                        toastType: 4,
-                        toastMessage: 'Token Error. ' + err
-                    })
-                }
-            } else if (decoded) {
-                User.findOne({_id: decoded._id}, (err, obj) => {
-                    if (obj && obj.accessToken === accessToken) {
-                        next();
+    if (req.headers) {
+        // debugger
+        const authHeader = req.headers['authorization'];
+        if (typeof authHeader !== undefined) {
+            const bearer = authHeader.split(' ');
+            const accessToken = bearer[1];
+            
+            jwt.verify(accessToken, config.get('jwtSecret'), (err, decoded) => {
+                if (err) {
+                    if (err.name === 'TokenExpiredError') {
+                        res.status(403).json({
+                            toastType: 4,
+                            toastMessage: 'Token expired Error'
+                        })
                     } else {
                         res.status(403).json({
                             toastType: 4,
-                            toastMessage: 'Authorization Error.'
-                        });
+                            toastMessage: 'Token Error. ' + err
+                        })
                     }
-                })
-            }
-        })
-    } else {
-        res.status(403).json({
-            toastType: 2,
-            toastMessage: 'Authorization logout'
-        })
+                } else if (decoded) {
+                    User.findOne({_id: decoded._id}, (err, obj) => {
+                        if (obj && obj.accessToken === accessToken) {
+                            next();
+                        } else {
+                            res.status(403).json({
+                                toastType: 4,
+                                toastMessage: 'Authorization Error.'
+                            });
+                        }
+                    })
+                }
+            })
+        } else {
+            res.status(403).json({
+                toastType: 2,
+                toastMessage: 'Authorization logout'
+            })
+        }
     }
+    
 }
 
 module.exports = checkToken
